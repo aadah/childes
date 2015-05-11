3d7e9ca1a0dcb7b75aef91d396a682b28d3d315b
Abdi-Hakin Dirie <abdihd@mit.edu>
2015-05-11 10:18:44 -0400

    modified init

--- src/init.scm
+++ src/init.scm
@@ -11,16 +11,28 @@
 ;;; Initializing the inverted index
 (define *ii* (make-inverted-index))
 
+(define (reset)
+  (set! *ii* (make-inverted-index)))
+
+#|
 ; Alice in Wonderland, by Lewis Carroll
 (ii/add-doc! *ii* (create-plain-doc "alice.txt"))
 ; Bible (King James Version)
 ;(ii/add-doc! *ii* (create-plain-doc "bible.txt"))
 ; Paradise Lost, by John Milton
-;(ii/add-doc! *ii* (create-plain-doc "paradise.txt"))
+(ii/add-doc! *ii* (create-plain-doc "paradise.txt"))
 ; The Republic, by Plato
-;(ii/add-doc! *ii* (create-plain-doc "republic.txt"))
+(ii/add-doc! *ii* (create-plain-doc "republic.txt"))
 ; The Adventures of Sherlock Holmes, by Arthur Conan Doyle
-;(ii/add-doc! *ii* (create-plain-doc "sherlock.txt"))
+(ii/add-doc! *ii* (create-plain-doc "sherlock.txt"))
 ; The Time Machine, by H. G. (Herbert George) Wells
 (ii/add-doc! *ii* (create-plain-doc "time.txt"))
+|#
 
+(ii/add-doc! *ii* (create-ling-doc "english-na-mor/Bates/Free20/amy20.cha"))
+(ii/add-doc! *ii* (create-ling-doc "english-na-mor/Bates/Free20/betty20.cha"))
+(ii/add-doc! *ii* (create-ling-doc "english-na-mor/Bates/Free20/chuck20.cha"))
+
+;(define (init type)
+;  (let ((docs (parser type)))
+;    (inverted-index-maker *ii* docs)))
